6be957d9f6febb4eb583e20195d9e83b924ed815
Michael Williams <michaelwilliams@SDMacBookPro2.local>
2016-10-02 08:21:50 -0600

    Fully working version 1.1, comments for code will follow

--- src/cashregister/MakeChange.java
+++ src/cashregister/MakeChange.java
@@ -15,7 +15,7 @@ public class MakeChange {
 		String tenderAgain;
 
 		System.out.print("Enter the amount of money tendered: ");
-		if( !(input.hasNextFloat()) || (amountTendered = input.nextFloat()) < 0.00F ){
+		if( !(input.hasNextFloat()) || (amountTendered = input.nextFloat()) < 0.00F ) {
 			System.err.println("Invalid amount entered.");
 			moneyTendered(itemPrice);
 		}
@@ -50,8 +50,7 @@ public class MakeChange {
 		String tenderAgain;
 		System.out.print("Would you like to process another transaction? (Y or N): ");
 		if( (tenderAgain = input.next().toUpperCase()).equals("Y") ) {
-				requestPrice();
-				input.close();
+				requestPrice(); 
 				return;
 			}
 			else if( (tenderAgain.equals("N")) ){
@@ -62,93 +61,56 @@ public class MakeChange {
 				System.err.println("Invalid response.");
 				requestPrice();
 				return;
-			}
-		
+			}		
 	}
 	
 	public static void calculateChange(float change){
-		//int twenty = 0; ten = 0, five = 0, one = 0, quarter = 0, dime = 0, nickel = 0, penny = 0;
-		float sum = change;
-		/*for ( ; penny < change/.01F; penny++) {
-			for ( ; nickel < change/.05F; nickel++) {
-				for ( ; dime < change/.10F; dime++) {
-					for ( ; quarter < change/.25F; quarter++){
-						for ( ; one < change/1.00F; one++ ){
-							for ( ; five < change/5.00F; five++ ){
-								for ( ; ten < change/10.00F ; ){
-									
-									for ( int twenty = 0 ; twenty < (int)(sum/20.00F); twenty++, sum = (int)(((sum%20.00F)+.005F)*100F)/100F){											
-											System.out.println(twenty + " " + sum);
-											if(twenty > 1) displayChange( twenty, " twenty dollar bills", sum);
-											else displayChange( twenty, " twenty dollar bill", sum);
-											if(sum == 0) return;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}*/
-		
-		if(((int)(sum/20.00F)) > 0){
-			int twenty = (int)(sum/20.00F);
-			sum = (int)(((sum%20.00F)+.005F)*100F)/100F;
-			if(twenty > 1) displayChange( twenty, " twenty dollar bills", sum);
-			else displayChange( twenty, " twenty dollar bill", sum);
-		}
-		if( ((int)(sum/10.00F)) > 0 ){
-			int ten = (int)(sum/10.00F);
-			sum = (int)(((sum%10.00F)+.005F)*100F)/100F;
-			if(ten > 1)displayChange( ten, " ten dollar bills", sum);
-			else displayChange( ten, " ten dollar bill", sum);
-		}
-		if( ((int)(sum/5.00F) > 0 ) ){
-			int five = (int)(sum/5.00F);
-			sum = (int)(((sum%5.00F)+.005F)*100F)/100F;
-			if(five > 1)displayChange( five, " five dollar bills", sum);
-			else displayChange( five, " five dollar bill", sum);
-		}
-		if( (int)(sum/1.00F) > 0 ){
-			int one = (int)(sum/1.00F);
-			sum = (int)(((sum%1.00F)+.005F)*100F)/100F;
-			if(one > 1)displayChange( one, " one dollar bills", sum);
-			else displayChange( one, " one dollar bill", sum);
-		}
-		if( (int)(sum/.25F) > 0 ){
-			int quarter = (int)(sum/.25F);
-			sum = (int)(((sum%.25F)+.005F)*100F)/100F;
-			if(quarter > 1)displayChange( quarter, " quarters", sum);
-			else displayChange( quarter, " quarter", sum);
-		}
-		if( ((int)(sum/.10F)) > 0 ){
-			int dime = (int)(sum/.10F);
-			sum = (int)(((sum%.10F)+.005F)*100F)/100F;
-			if(dime > 1)displayChange( dime, " dimes", sum);
-			else displayChange( dime, " dime", sum);
-		}
-		if( (int)(sum/.05F) > 0 ){
-
-			int nickel = (int)(sum/.05F);
-			sum = (int)(((sum%.05F)+.005F)*100F)/100F;
-			if(nickel > 1)displayChange( nickel, " nickels", sum);
-			else displayChange( nickel, " nickel", sum);
-		}
-		if( sum > 0F ){			
-			int penny = (int)(sum/.01F);
-			if(penny > 1)displayChange( penny, " pennies", 0F);
-			else displayChange( penny, " penny", 0F);
-		}
+			
+		float sum = change; // 
 	
+		for(int i = 0, k = 0, m = 0; i < 8 && sum > 0; i++){
+			if(k > 0) m = k;
+			switch(i){
+			case 0:
+				if((k = ((int)(sum/20.00F))) == 1) displayChange(k, " twenty dollar bill", (sum = (int)(((sum%20.00F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/20.00F))) > 1) displayChange(k, " twenty dollar bills", (sum = (int)(((sum%20.00F)+.005F)*100F)/100F), m);
+				break;
+			case 1:
+				if((k = ((int)(sum/10.00F))) == 1) displayChange(k, " ten dollar bill", (sum = (int)(((sum%10.00F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/10.00F))) > 1) displayChange(k, " ten dollar bills", (sum = (int)(((sum%10.00F)+.005F)*100F)/100F), m);
+				break;
+			case 2:
+				if((k = ((int)(sum/5.00F))) == 1) displayChange(k, " five dollar bill", (sum = (int)(((sum%5.00F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/5.00F))) > 1) displayChange(k, " five dollar bills", (sum = (int)(((sum%5.00F)+.005F)*100F)/100F), m);
+				break;
+			case 3:
+				if((k = ((int)(sum/1.00F))) == 1) displayChange(k, " one dollar bill", (sum = (int)(((sum%1.00F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/1.00F))) > 1) displayChange(k, " one dollar bills", (sum = (int)(((sum%1.00F)+.005F)*100F)/100F), m);
+				break;
+			case 4:
+				if((k = ((int)(sum/.25F))) == 1) displayChange(k, " quarter", (sum = (int)(((sum%.25F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/.25F))) > 1) displayChange(k, " quarters", (sum = (int)(((sum%.25F)+.005F)*100F)/100F), m);
+				break;
+			case 5:
+				if((k = ((int)(sum/.10F))) == 1) displayChange(k, " dime", (sum = (int)(((sum%.10F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/.10F))) > 1) displayChange(k, " dimes", (sum = (int)(((sum%.10F)+.005F)*100F)/100F), m);
+				break;
+			case 6:
+				if((k = ((int)(sum/.05F))) == 1) displayChange(k, " nickel", (sum = (int)(((sum%.05F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/.05F))) > 1) displayChange(k, " nickels", (sum = (int)(((sum%.05F)+.005F)*100F)/100F), m);
+			case 7:
+				if((k = ((int)(sum/.01F))) == 1) displayChange(k, " penny", (sum = (int)(((sum%.01F)+.005F)*100F)/100F), m);				
+				else if ((k = ((int)(sum/.01F))) > 1) displayChange(k, " pennies", (sum = (int)(((sum%.01F)+.005F)*100F)/100F), m);
+			default: break;
+			}
+		}
 
-		//displayChange(twenty, ten, five, one, quarter, dime, nickel, penny);
-		//System.out.println(twenty + " twenty/twenties, " + ten + " ten(s), " + five + " five(s), " + one + " one(s), " + 
-			//			   quarter + " quarter(s), " + dime + " dime(s), " + nickel + " nickel(s), and " + penny + " penny/pennies.");
 	}
 	
-	public static void displayChange(int numCurrency, String nameCurrency, float total){
+	public static void displayChange(int numCurrency, String nameCurrency, float total, int previousCurrency){
+		
 		if(total > 0) System.out.print(numCurrency + nameCurrency + ", ");
+		else if(previousCurrency > 0 && total == 0) System.out.println("and " + numCurrency + nameCurrency + ".");
 		else System.out.println(numCurrency + nameCurrency + ".");
 	}
-
 }
