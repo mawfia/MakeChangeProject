d50311276e80845ad3acea84cba8abe9a646aca8
Michael Williams <michaelwilliams@SDMacBookPro2.local>
2016-10-02 19:44:49 -0600

    Fully working version 1.2, fixed bug in requestPrice method and updated comments.

--- README.md
+++ README.md
@@ -6,14 +6,16 @@ This class calculates amount of change in USD currency after prompting user for 
 The result is a message telling the user exact amount of dollars bills and coins using the largest dollar/coin possible (i.e. $10.00 in change
 will result in one $10.00 bill not one $5.00 bill and five one $1.00 bills etc.).
 
+Program accepts decimal (beyond two digits) and whole, as well as negative and positive numbers.  However, in order for the code to correctly process transactions price and amount tendered should be in true dollar and coin amounts, otherwise user will be prompted to enter in a valid dollar coin amount, or cancel the transaction.  Values entered with greater than 2 decimal places will be truncated to 2.  Responses to prompts can be in lowercase or uppercase and should be with the provided choices "y" of "n".
+
 This class is comprised of four methods: main, requestPrice, moneyTendered, calculateChange, and displayChange.
 
 public static void main(String[] args):
 This methods only function is to call method requestPrice().
 
-public static void requestPrice():
+public static boolean requestPrice():
 -----------------------------------------------
-This method accepts and returns no data.
+This method accepts no data, and returns true once complete processing a new transaction.
 
 1.  Prompts user for the price of the item purchased.
 2.  Tests to see if the amount entered for price is valid.  
--- src/cashregister/MakeChange.java
+++ src/cashregister/MakeChange.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-		
+
 		requestPrice();
-		
+
 	}
 	public static void moneyTendered(float itemPrice){
 		Scanner input = new Scanner(System.in);
@@ -20,95 +20,97 @@ public class MakeChange {
 			moneyTendered(itemPrice);
 		}
 		else if(amountTendered == itemPrice) System.out.println("Customer provided the exact amount. No change due.");
-		else if(amountTendered < itemPrice){ 
-			System.out.println("Customer provided too little money."); 
-			
+		else if(amountTendered < itemPrice){
+			System.out.println("Customer provided too little money.");
+
 			do{
 				System.out.print("Does the customer still want to purchase the item? (Y or N): ");
 				if( (tenderAgain = input.next().toUpperCase()).equals("Y") ) {
-					moneyTendered(itemPrice); 
+					moneyTendered(itemPrice);
 					return;
 				}
 				else if( (tenderAgain.equals("N")) ) return;
 				else System.err.println("Invalid response.");
-			} while( true ) ;
+			} while( true );
 		}
 		else calculateChange((int)(((amountTendered - itemPrice)+.005F)*100F)/100F);
 	}
-	
-	public static void requestPrice(){
+
+	public static boolean requestPrice(){
 		Scanner input = new Scanner(System.in);
 		float price;
-				
+		boolean newTransaction = true;
+
 		System.out.print("Enter the the price of the item: ");
 		if( !(input.hasNextFloat()) || (price = input.nextFloat()) < 0.00F ){
 				System.err.println("Invalid price entered.");
-				requestPrice();
+				newTransaction = requestPrice();
 		}
 		else moneyTendered(price);
-		
+
 		String tenderAgain;
-		System.out.print("Would you like to process another transaction? (Y or N): ");
-		if( (tenderAgain = input.next().toUpperCase()).equals("Y") ) {
-				requestPrice(); 
-				return;
+		if(newTransaction){
+			System.out.print("Would you like to process another transaction? (Y or N): ");
+			if( (tenderAgain = input.next().toUpperCase()).equals("Y") ) {
+				newTransaction = requestPrice();
 			}
 			else if( (tenderAgain.equals("N")) ){
 				input.close();
-				return;
+				return false;
 			}
 			else {
 				System.err.println("Invalid response.");
-				requestPrice();
-				return;
-			}		
+				newTransaction = requestPrice();
+			}
+		}
+		return false;
 	}
-	
+
 	public static void calculateChange(float change){
-			
-		float sum = change; // 
-	
+
+		float sum = change; //
+
 		for(int i = 0, k = 0, m = 0; i < 8 && sum > 0; i++){
 			if(k > 0) m = k;
 			switch(i){
 			case 0:
-				if((k = ((int)(sum/20.00F))) == 1) displayChange(k, " twenty dollar bill", (sum = (int)(((sum%20.00F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/20.00F))) == 1) displayChange(k, " twenty dollar bill", (sum = (int)(((sum%20.00F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/20.00F))) > 1) displayChange(k, " twenty dollar bills", (sum = (int)(((sum%20.00F)+.005F)*100F)/100F), m);
 				break;
 			case 1:
-				if((k = ((int)(sum/10.00F))) == 1) displayChange(k, " ten dollar bill", (sum = (int)(((sum%10.00F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/10.00F))) == 1) displayChange(k, " ten dollar bill", (sum = (int)(((sum%10.00F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/10.00F))) > 1) displayChange(k, " ten dollar bills", (sum = (int)(((sum%10.00F)+.005F)*100F)/100F), m);
 				break;
 			case 2:
-				if((k = ((int)(sum/5.00F))) == 1) displayChange(k, " five dollar bill", (sum = (int)(((sum%5.00F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/5.00F))) == 1) displayChange(k, " five dollar bill", (sum = (int)(((sum%5.00F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/5.00F))) > 1) displayChange(k, " five dollar bills", (sum = (int)(((sum%5.00F)+.005F)*100F)/100F), m);
 				break;
 			case 3:
-				if((k = ((int)(sum/1.00F))) == 1) displayChange(k, " one dollar bill", (sum = (int)(((sum%1.00F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/1.00F))) == 1) displayChange(k, " one dollar bill", (sum = (int)(((sum%1.00F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/1.00F))) > 1) displayChange(k, " one dollar bills", (sum = (int)(((sum%1.00F)+.005F)*100F)/100F), m);
 				break;
 			case 4:
-				if((k = ((int)(sum/.25F))) == 1) displayChange(k, " quarter", (sum = (int)(((sum%.25F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/.25F))) == 1) displayChange(k, " quarter", (sum = (int)(((sum%.25F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/.25F))) > 1) displayChange(k, " quarters", (sum = (int)(((sum%.25F)+.005F)*100F)/100F), m);
 				break;
 			case 5:
-				if((k = ((int)(sum/.10F))) == 1) displayChange(k, " dime", (sum = (int)(((sum%.10F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/.10F))) == 1) displayChange(k, " dime", (sum = (int)(((sum%.10F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/.10F))) > 1) displayChange(k, " dimes", (sum = (int)(((sum%.10F)+.005F)*100F)/100F), m);
 				break;
 			case 6:
-				if((k = ((int)(sum/.05F))) == 1) displayChange(k, " nickel", (sum = (int)(((sum%.05F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/.05F))) == 1) displayChange(k, " nickel", (sum = (int)(((sum%.05F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/.05F))) > 1) displayChange(k, " nickels", (sum = (int)(((sum%.05F)+.005F)*100F)/100F), m);
 			case 7:
-				if((k = ((int)(sum/.01F))) == 1) displayChange(k, " penny", (sum = (int)(((sum%.01F)+.005F)*100F)/100F), m);				
+				if((k = ((int)(sum/.01F))) == 1) displayChange(k, " penny", (sum = (int)(((sum%.01F)+.005F)*100F)/100F), m);
 				else if ((k = ((int)(sum/.01F))) > 1) displayChange(k, " pennies", (sum = (int)(((sum%.01F)+.005F)*100F)/100F), m);
 			default: break;
 			}
 		}
 
 	}
-	
+
 	public static void displayChange(int numCurrency, String nameCurrency, float total, int previousCurrency){
-		
+
 		if(total > 0) System.out.print(numCurrency + nameCurrency + ", ");
 		else if(previousCurrency > 0 && total == 0) System.out.println("and " + numCurrency + nameCurrency + ".");
 		else System.out.println(numCurrency + nameCurrency + ".");
